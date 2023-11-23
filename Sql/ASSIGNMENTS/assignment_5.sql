79da3736fda844dd0677fe964ef15a4a0c11be66
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-11-23 23:12:03 +0530

    updates

--- Sql/ASSIGNMENTS/assignment_5.sql
+++ Sql/ASSIGNMENTS/assignment_5.sql
@@ -1,17 +1,4 @@
--- Create a sample table
-CREATE TABLE odd_table (
-  id INT,
-  data VARCHAR(50)
-);
-
--- Insert some sample data
-INSERT INTO odd_table (id, data)
-VALUES
-  (1, 'Record 1'),
-  (2, 'Record 2'),
-  (3, 'Record 3'),
-  (4, 'Record 4'),
-  (5, 'Record 5');
-
--- Retrieve odd records
-SELECT * FROM odd_table WHERE id % 2 = 1;
+-- Retrieve common records between two tables without using JOIN
+SELECT id, data FROM odd_table
+INTERSECT
+SELECT id, data FROM even_table;
